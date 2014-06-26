905351139e94fcbe3c273e4f7c2f03acd775c75f
Jordan Hudgens <jordan@hudgens.com>
2014-06-26 16:46:11 -0500

    added prolog connector for input and output to prolog file

--- survey/ptest.py
+++ survey/ptest.py
@@ -1,13 +1,13 @@
-from pyswip import Functor, Variable, Query, call
+#from pyswip import Functor, Variable, Query, call
 
-assertz = Functor("assertz", 1)
-father = Functor("father", 2)
+#assertz = Functor("assertz", 1)
+#father = Functor("father", 2)
 
-call(assertz(father("michael","john")))
-call(assertz(father("michael","gina")))
+#call(assertz(father("michael","john")))
+#call(assertz(father("michael","gina")))
 
-X = Variable()
-q = Query(father("michael",X))
-while q.nextSolution():
-      print "Hello,", X.value
-q.closeQuery()
+#X = Variable()
+#q = Query(father("michael",X))
+#while q.nextSolution():
+      #print "Hello,", X.value
+#q.closeQuery()
