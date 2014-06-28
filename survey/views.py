008b7ecc9691170a0895cd11630dd28401abceef
Jordan Hudgens <jordan@hudgens.com>
2014-06-28 13:28:42 -0500

    added string matching for the result page UI

--- survey/views.py
+++ survey/views.py
@@ -99,15 +99,118 @@ def Confirm(request, uuid):
     ## ************** CHANGE PROLOG TERMS TO LINKS *************** ##
     
     if soln["X"] == "thinkfulfrontend":
-      soln["X"] = "<a href='http://www.thinkful.com/'>Thinkful - Front End Development</a>"
+      soln["X"] = "<a href='http://www.thinkful.com/' target='_blank'>Thinkful - Front End Development</a>"
       
     if soln["X"] == "thinkfulpython":
-      soln["X"] = "<a href='http://www.thinkful.com/'>Thinkful - Python Web Development</a>"
+      soln["X"] = "<a href='http://www.thinkful.com/' target='_blank'>Thinkful - Python Web Development</a>"
 
     if soln["X"] == "thinkfulruby":
-      soln["X"] = "<a href='http://www.thinkful.com/'>Thinkful - Ruby on Rails Web Development</a>"
+      soln["X"] = "<a href='http://www.thinkful.com/' target='_blank'>Thinkful - Ruby on Rails Web Development</a>"
+      
+    if soln["X"] == "thinkfulios":
+      soln["X"] = "<a href='http://www.thinkful.com/' target='_blank'>Thinkful - iOS Mobile App Development</a>"
+      
+    if soln["X"] == "blocror":
+      soln["X"] = "<a href='https://www.bloc.io/web-development' target='_blank'>Bloc - Ruby on Rails Web Development</a>"
+      
+    if soln["X"] == "blocfrontend":
+      soln["X"] = "<a href='https://www.bloc.io/front-end-development' target='_blank'>Bloc - Front End Development</a>"
 
+    if soln["X"] == "blocandroid":
+      soln["X"] = "<a href='https://www.bloc.io/android' target='_blank'>Bloc - Android Mobile App Development</a>"
+      
+    if soln["X"] == "blocois":
+      soln["X"] = "<a href='https://www.bloc.io/ios' target='_blank'>Bloc - iOS Mobile App Development</a>"
+      
+    if soln["X"] == "codementorpython":
+      soln["X"] = "<a href='https://www.codementor.io/learn-python-online' target='_blank'>CodeMentor - Python Web Development</a>"
+      
+    if soln["X"] == "codementorror":
+      soln["X"] = "<a href='https://www.codementor.io/learn-ruby-on-rails-online' target='_blank'>CodeMentor - Ruby on Rails Web Development</a>"
+      
+    if soln["X"] == "codementorios":
+      soln["X"] = "<a href='https://www.codementor.io/learn-ios-development' target='_blank'>CodeMentor - iOS Mobile App Development</a>"
 
+    if soln["X"] == "codementorandroid":
+      soln["X"] = "<a href='https://www.codementor.io/learn-android-development' target='_blank'>CodeMentor - Android Mobile App Development</a>"
+      
+    if soln["X"] == "codementorfrontend":
+      soln["X"] = "<a href='https://www.codementor.io/learn-html' target='_blank'>CodeMentor - Front End Development</a>"
+      
+    if soln["X"] == "codechef":
+      soln["X"] = "<a href='http://www.codechef.com/' target='_blank'>CodeChef - Competitive Coding Challenges</a>"
+      
+    if soln["X"] == "codecademyhtml":
+      soln["X"] = "<a href='http://www.codecademy.com/tracks/web' target='_blank'>Codecademy - HTML Training Track</a>"
+      
+    if soln["X"] == "codecademyjquery":
+      soln["X"] = "<a href='http://www.codecademy.com/tracks/jquery' target='_blank'>Codecademy - jQuery Training Track</a>"
+
+    if soln["X"] == "codecademyror":
+      soln["X"] = "<a href='http://www.codecademy.com/tracks/ruby' target='_blank'>Codecademy - Ruby Programming Track</a>"
+      
+    if soln["X"] == "codecademypython":
+      soln["X"] = "<a href='http://www.codecademy.com/tracks/python' target='_blank'>Codecademy - Python Programming Track</a>"
+      
+    if soln["X"] == "codecademyphp":
+      soln["X"] = "<a href='http://www.codecademy.com/tracks/php' target='_blank'>Codecademy - PHP Programming Track</a>"
+      
+    if soln["X"] == "codeschoolror":
+      soln["X"] = "<a href='https://www.codeschool.com/paths/ruby' target='_blank'>CodeSchool - Ruby on Rails Development</a>"
+      
+    if soln["X"] == "codeschoolfrontend":
+      soln["X"] = "<a href='https://www.codeschool.com/paths/html-css' target='_blank'>CodeSchool - Front End Development</a>"
+      
+    if soln["X"] == "codeschoolios":
+      soln["X"] = "<a href='https://www.codeschool.com/paths/ios' target='_blank'>CodeSchool - iOS Mobile App Development</a>"
+      
+    if soln["X"] == "rubymonk":
+      soln["X"] = "<a href='https://rubymonk.com/' target='_blank'>Ruby Monk - Ruby Programming Training</a>"
+      
+    if soln["X"] == "treehousefrontend":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:html' target='_blank'>Team Treehouse - Front End Development</a>"
+      
+    if soln["X"] == "treehouseror":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:ruby' target='_blank'>Team Treehouse - Ruby on Rails Development</a>"
+      
+    if soln["X"] == "treehousephp":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:php' target='_blank'>Team Treehouse - PHP Web Development</a>"
+      
+    if soln["X"] == "treehouseandroid":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:android' target='_blank'>Team Treehouse - Android Mobile App Development</a>"
+      
+    if soln["X"] == "treehouseios":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:ios' target='_blank'>Team Treehouse - iOS Mobile App Development</a>"
+      
+    if soln["X"] == "udacitypython1":
+      soln["X"] = "<a href='https://www.udacity.com/course/cs101' target='_blank'>Udacity - Intro to Python Web Development</a>"
+      
+    if soln["X"] == "udacitypython2":
+      soln["X"] = "<a href='http://teamtreehouse.com/library/topic:android' target='_blank'>Udacity - Intermediate Python Web Development</a>"
+      
+    if soln["X"] == "udacityfrontend":
+      soln["X"] = "<a href='https://www.udacity.com/course/ud248' target='_blank'>Udacity - Front End Development</a>"
+      
+    if soln["X"] == "javaposse":
+      soln["X"] = "<a href='http://javaposse.com/' target='_blank'>Android Podcasts</a>"
+      
+    if soln["X"] == "stackexchange":
+      soln["X"] = "<a href='http://blog.stackoverflow.com/category/podcasts/' target='_blank'>StackExchange - Programming Podcasts</a>"
+      
+    if soln["X"] == "hansel":
+      soln["X"] = "<a href='http://www.hanselminutes.com/' target='_blank'>Hansel Minutes - Programming Podcasts</a>"
+      
+    if soln["X"] == "twittv":
+      soln["X"] = "<a href='http://twit.tv/sn' target='_blank'>Team Twit.tv - Programming Podcasts</a>"
+      
+    if soln["X"] == "thechangelog":
+      soln["X"] = "<a href='http://thechangelog.com/podcast/' target='_blank'>The Change Log - Open Source Podcasts</a>"
+      
+    if soln["X"] == "phptownhall":
+      soln["X"] = "<a href='http://phptownhall.com/' target='_blank'>PHP Town Hall - PHP Programming Podcasts</a>"
+      
+    if soln["X"] == "rubyrogues":
+      soln["X"] = "<a href='http://rubyrogues.com/' target='_blank'>Ruby Rogues - Ruby Programming Podcasts</a>"
 
     
     ## *************************** END *************************** ##
